05961587a02eb3e2e515eefc6e2022d6dae5ec42
Sakthivel-coder12 <sakthivel.m2023@vitstudent.ac.in>
2025-07-07 18:22:14 +0530

    app

--- app.py
+++ app.py
@@ -28,8 +28,6 @@ def predict_datapoint():
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('reading_score')),
             writing_score=float(request.form.get('writing_score'))
-
-
         )
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
@@ -44,5 +42,3 @@ def predict_datapoint():
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",debug=True)        
-
-
